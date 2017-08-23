7497507bb3ea758ac83ab2dfbd8b8d7e4991e15d
mohanputtanna <mohanp062@gmail.com>
2017-08-24 01:09:27 +0530

    read data implemented

--- src/main/scala/df/game/GameAvgRating.scala
+++ src/main/scala/df/game/GameAvgRating.scala
@@ -5,9 +5,9 @@ import org.apache.spark.sql.SparkSession
 object GameAvgRating {
 
   def main(args: Array[String]): Unit = {
-    val spark = SparkSession.builder().getOrCreate()
-
-    val inputData = spark.
+    val spark = SparkSession.builder().appName("STB_Data_Analysis").master("local[2]").getOrCreate()
+    val inputData = spark.read.csv("/home/mohan/Dataflair/gamedata/ign.csv")
+    inputData.show(10)
   }
 
 }
